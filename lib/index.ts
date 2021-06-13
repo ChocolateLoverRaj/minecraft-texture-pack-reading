edc9fcdd6b071723fb72371dfe2b1deb7fbcf10f
Rajas Paranjpe <paranjperajas@gmail.com>
2021-06-13 16:40:56 -0500

    Food

--- lib/index.ts
+++ lib/index.ts
@@ -95,6 +95,59 @@ const categories: Category[] = [{
     'netherite_scrap',
     'redstone'
   ]
+}, {
+  name: 'food',
+  model: {
+    parent: 'minecraft:item/generated',
+    textures: {
+      layer0: `minecraft:${customTexturePack}/food`
+    }
+  },
+  texture: join(resDir, 'food.png'),
+  items: [
+    'apple',
+    'baked_potato',
+    'beetroot',
+    'beetroot_soup',
+    'bread',
+    'cake',
+    'cake',
+    'carrot',
+    'chorus_fruit',
+    'cooked_chicken',
+    'cooked_cod',
+    'cooked_mutton',
+    'cooked_porkchop',
+    'cooked_rabbit',
+    'cooked_salmon',
+    'cookie',
+    'dried_kelp',
+    'enchanted_golden_apple',
+    'golden_apple',
+    'glow_berries',
+    'golden_carrot',
+    'melon_slice',
+    'mushroom_stew',
+    'poisonous_potato',
+    'potato',
+    'pufferfish',
+    'pumpkin_pie',
+    'rabbit_stew',
+    'beef',
+    'chicken',
+    'cod',
+    'mutton',
+    'porkchop',
+    'rabbit',
+    'salmon',
+    'rotten_flesh',
+    'spider_eye',
+    'cooked_beef',
+    'suspicious_stew',
+    'suspicious_stew',
+    'sweet_berries',
+    'tropical_fish'
+  ]
 }];
 
 (async () => {
