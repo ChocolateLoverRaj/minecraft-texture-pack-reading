9d1e93af2d5fbf5dbc12d44b7cba14579115e44f
Rajas Paranjpe <paranjperajas@gmail.com>
2021-06-13 16:19:59 -0500

    armor and ore items

--- lib/index.ts
+++ lib/index.ts
@@ -16,28 +16,35 @@ const vanillaModelsDir = join(modelsDir, 'item')
 const customTexturesDir = join(minecraftDir, 'textures', customTexturePack)
 const toolTypes = ['wooden', 'stone', 'golden', 'iron', 'diamond', 'netherite'] as const
 const tools = ['shovel', 'pickaxe', 'axe', 'hoe'] as const
+// FIXME: Leather armor texture change
+const armorTypes = [/* 'leather', */'chainmail', 'iron', 'diamond', 'golden', 'netherite'] as const
+const armors = ['helmet', 'chestplate', 'leggings', 'boots'] as const
 
 interface Category {
-  items: string[]
+  items: readonly string[]
   texture: string
   name: string
   model: object
 }
 
-const itemModel = {
-  parent: 'minecraft:item/handheld',
-  textures: {
-    layer0: `minecraft:${customTexturePack}/tool`
-  }
-}
 const categories: Category[] = [{
   name: 'tool',
-  model: itemModel,
+  model: {
+    parent: 'minecraft:item/handheld',
+    textures: {
+      layer0: `minecraft:${customTexturePack}/tool`
+    }
+  },
   texture: join(resDir, 'tool.png'),
   items: multiplyArrays(toolTypes, tools).map(([type, tool]) => `${type}_${tool}`)
 }, {
   name: 'bucket',
-  model: itemModel,
+  model: {
+    parent: 'minecraft:item/handheld',
+    textures: {
+      layer0: `minecraft:${customTexturePack}/bucket`
+    }
+  },
   texture: join(resDir, 'bucket.png'),
   items: [
     'bucket',
@@ -51,6 +58,43 @@ const categories: Category[] = [{
     'tropical_fish_bucket',
     'axolotl_bucket'
   ]
+}, ...armors.map<Category>(armor => ({
+  name: armor,
+  model: {
+    parent: 'minecraft:item/generated',
+    textures: {
+      layer0: `minecraft:${customTexturePack}/${armor}`
+    }
+  },
+  texture: join(resDir, `${armor}.png`),
+  items: armorTypes.map(type => `${type}_${armor}`)
+})), {
+  name: 'ore',
+  model: {
+    parent: 'minecraft:item/generated',
+    textures: {
+      layer0: `minecraft:${customTexturePack}/ore`
+    }
+  },
+  texture: join(resDir, 'ore.png'),
+  items: [
+    'coal',
+    'charcoal',
+    'diamond',
+    'emerald',
+    'lapis_lazuli',
+    'quartz',
+    'amethyst_shard',
+    'raw_iron',
+    'iron_ingot',
+    'raw_copper',
+    'copper_ingot',
+    'raw_gold',
+    'gold_ingot',
+    'netherite_ingot',
+    'netherite_scrap',
+    'redstone'
+  ]
 }];
 
 (async () => {
@@ -61,13 +105,8 @@ const categories: Category[] = [{
     mkdir(minecraftDir, { recursive: true }).then(async () => await Promise.all([
       mkdir(modelsDir)
         .then(async () => await Promise.all([
-          mkdir(customModelsDir).then(async () => await Promise.all(categories.map(async ({ name }) =>
-            await writeFile(join(customModelsDir, `${name}.json`), {
-              parent: 'minecraft:item/handheld',
-              textures: {
-                layer0: `minecraft:${customTexturePack}/${name}`
-              }
-            }, { spaces: 2 })))),
+          mkdir(customModelsDir).then(async () => await Promise.all(categories.map(async ({ name, model }) =>
+            await writeFile(join(customModelsDir, `${name}.json`), model, { spaces: 2 })))),
           mkdir(vanillaModelsDir).then(async () => await Promise.all(categories.map(async ({ items, name }) =>
             await Promise.all(items.map(async item => await writeFile(join(vanillaModelsDir, `${item}.json`), {
               parent: `minecraft:item_${customTexturePack}/${name}`
